daa895b4f270a202012f851c757865f2b10e2aff
Ankur Thakur <thakur.ankur049@gmail.com>
2018-06-05 12:17:13 +0530

    Few commits.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,47 @@ import Board from "./Board/Board";
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { rows: 10, cols: 10 }
+    let settings = {
+      rows: 10,
+      cols: 10,
+    }
+    this.state.settings = settings;
+    this.gameOverCounter = 0;
+  }
+
+  createGrid(rows, cols) {
+    let matrix = [];
+    for (let i = 0; i < rows; i++) {
+      matrix[i] = [];
+      for (let j = 0; j < cols; j++) {
+        matrix[i][j] = 'unset';
+      }
+    }
+    return matrix;
+  }
+
+  updateGridValue(rows, cols, value) {
+    this.gameOverCounter++;
+    this.setState(function (prevState, props) {
+      prevState.grid[rows][cols] = value;
+      return { grid: prevState.grid }
+    })
+  }
+
+  checkForGameOver(){
+    if(this.gameOverCounter >= rows * cols){
+      alert("GAME OVER");
+    }
+  }
+
+  
+
+  componentWillMount() {
+    this.state.grid = this.createGrid(this.state.rows, this.state.cols);
+  }
   render() {
     return (
       <div className="App">
@@ -13,7 +54,7 @@ class App extends Component {
           Two players. Toggle between S and O keywords.
         </p>
         <main>
-          <Board rows="10" cols="10" />
+          <Board {...this.state.settings} />
         </main>
       </div>
     );
